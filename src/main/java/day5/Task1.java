f6449bbebc299ae92831fa45f36b4b0dad7ca00d
vadim157 <trofimets@inbox.ru>
2022-09-15 15:44:12 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,11 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setModel("BMW");
+        car.setColor("Black");
+        car.setYear(2015);
+        System.out.println("Модель: " + car.getModel() + "," + " Цвет: " + car.getColor() + "," + " Год выпуска: " + car.getYear());
 
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -4,5 +4,9 @@ package day5;
 public class Task2 {
     public static void main(String[] args) {
 
+        Motorbike motorbike = new Motorbike("Ducati","Red", 2018);
+        System.out.println("Модель: " + motorbike.getModel() + "," + " Цвет: " + motorbike.getColor() + "," + " Год выпуска: " + motorbike.getYear());
+
+
     }
 }
