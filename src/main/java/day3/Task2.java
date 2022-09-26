f553919e81cc1e0b8bca9f70849fa66358cb5a8e
vadim157 <trofimets@inbox.ru>
2022-09-26 14:25:02 +0300

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double a = scanner.nextDouble();
-        double b = scanner.nextDouble();
+        double a;
+        double b;
         boolean flag = true;
         while (flag) {
+            a = scanner.nextDouble();
+            b = scanner.nextDouble();
             if (b == 0) {
 //                flag = false;
                 break;
